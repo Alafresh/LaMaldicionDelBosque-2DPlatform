732ee558866c9925cfde9efdd5b76bf18e69f3ca
Alafresh <alafresh16@gmail.com>
2024-12-26 00:01:53 -0500

    Update game

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speedMovement = 10;
+    private Rigidbody2D _rb2d;
+
+    private void Awake()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        
+        Vector2 direction = new Vector2(x, y);
+        Walk(direction);
+    }
+
+    private void Walk(Vector2 direction)
+    {
+        _rb2d.linearVelocity = direction * speedMovement;
+    }
+}
